9ff23f9f639c7dd163a9e9175ebbf6151cd8869e
phattarintorn <32997337+phattarintorn@users.noreply.github.com>
2020-02-01 15:30:42 +0700

    camera profile

--- GOBALS.js
+++ GOBALS.js
@@ -1,3 +1,6 @@
 export default {
 	URL: 'http://191.1.1.93:5001/',
+	URL_UPLOAD: 'http://191.1.1.93:5001/upload-image/',
+	URL_DELETE: 'http://191.1.1.93:5001/delete-image/',
+	// 13.229.233.71 > only mobile internet
 }
